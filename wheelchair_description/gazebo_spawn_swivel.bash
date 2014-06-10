53e776312f1f91322b3d44005b08f3fbb10755a8
curranw <curranw@onid.orst.edu>
2014-06-10 12:42:05 -0700

    Turned wheelchair 90 degrees to fix odometry errors.

--- wheelchair_description/gazebo_spawn_swivel.bash
+++ wheelchair_description/gazebo_spawn_swivel.bash
@@ -8,6 +8,12 @@ roslaunch gazebo_ros empty_world.launch & fooPid=$!
 
 rosrun gazebo_ros spawn_model -file urdf/model.urdf -urdf -model wheelchair & barPid=$!
 
+sleep 10
+
+roslaunch wheelchair_description wheelchair_description.launch
+
+roslaunch ira_laser_tools laserscan_multi_merger.launch
+
 wait
 
 
