68b9045918c43749fad26586c90b75a4682dc478
curranw <curranw@onid.orst.edu>
2014-12-17 15:53:33 -0800

    URDF and launch fixes.

--- wheelchair_description/gazebo_spawn_swivel_willow.bash
+++ wheelchair_description/gazebo_spawn_swivel_willow.bash
@@ -1,6 +1,6 @@
 
 
-rosrun xacro xacro.py urdf/wheelchair_with_swivels.xacro > urdf/model.urdf
+rosrun xacro xacro.py urdf/wheelchair_gazebo.xacro > urdf/model.urdf
 
 trap 'kill $fooPid $barPid' EXIT
 
@@ -9,8 +9,6 @@ roslaunch gazebo_ros willowgarage_world.launch & fooPid=$!
 rosrun gazebo_ros spawn_model -file urdf/model.urdf -urdf -z 0.5 -model wheelchair & barPid=$!
 
 sleep 10
-
-roslaunch wheelchair_description wheelchair_description.launch
 
 wait
 
