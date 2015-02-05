df0bba12ef8835cec26618dcc129fc7e3c0051c3
Dan Lazewatsky <dan@lazewatsky.com>
2015-02-05 11:47:18 -0600

    configure environment for logging (so launch file can see log dir)

---
+++ wheelchair_bringup/env/setup.bash
@@ -0,0 +1,19 @@
+env_dir=$(dirname $BASH_SOURCE)
+dir=$(pwd)
+cd $env_dir
+
+source $env_dir/vars.bash
+
+# find the workspace's setup file
+while [ ! \( -e src -a -e src/CMakeLists.txt \) -a "$(pwd)" != "/" ] ; do
+  cd ..
+done
+
+if [ "$(pwd)" = "/" ]; then
+  echo "Not in catkin workspace" 1>&2
+else
+	source $(pwd)/devel/setup.bash
+fi
+
+cd $dir
+
