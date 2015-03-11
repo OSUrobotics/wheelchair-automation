2e828f59f99f1606f9284cc93efdb4e870b90d63
curranw <curranw@onid.orst.edu>
2015-03-11 14:32:42 -0700

    Cleaned up simulator spawn files to use the correct urdf models.

--- wheelchair_description/gazebo_spawn.bash
+++ wheelchair_description/gazebo_spawn.bash
@@ -1,10 +1,10 @@
 
 
-rosrun xacro xacro.py urdf/wheelchair.xacro > urdf/model.urdf
+rosrun xacro xacro.py urdf/wheelchair_gazebo.xacro > urdf/model.urdf
 
 trap 'kill $fooPid $barPid' EXIT
 
-roslaunch gazebo_ros empty_world.launch & fooPid=$!
+roslaunch gazebo_ros willowgarage_world.launch & fooPid=$!
 
 rosrun gazebo_ros spawn_model -file urdf/model.urdf -urdf -model wheelchair & barPid=$!
 
--- wheelchair_description/rviz_viz.bash
+++ wheelchair_description/rviz_viz.bash
@@ -1,3 +1,3 @@
-rosrun xacro xacro.py urdf/wheelchair.xacro > urdf/model.urdf
+rosrun xacro xacro.py urdf/wheelchair_gazebo.xacro > urdf/model.urdf
 
 roslaunch urdf_tutorial display.launch model:=urdf/model.urdf
