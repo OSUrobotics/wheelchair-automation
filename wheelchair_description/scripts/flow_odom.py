877e448ef11f7bf045d16d8eb16984ade7b8a027
Dan Lazewatsky <dan@lazewatsky.com>
2015-08-27 11:39:50 -0500

    publish odom->base_frame transform

--- wheelchair_description/scripts/flow_odom.py
+++ wheelchair_description/scripts/flow_odom.py
@@ -10,6 +10,10 @@ from copy import deepcopy
 import numpy as np
 from tf.transformations import quaternion_about_axis, euler_from_quaternion, quaternion_from_euler
 from tf_conversions import fromTf, toTf
+
+from urdf_parser_py.urdf import URDF
+from pykdl_utils.kdl_parser import kdl_tree_from_urdf_model
+from pykdl_utils.kdl_kinematics import KDLKinematics
 import PyKDL
 
 
@@ -47,6 +51,9 @@ class FlowTransformer(object):
         offset = self.listener.lookupTransform('odom_temp', self.flow_frame, rospy.Time())[0]
         self.x_t = np.array(offset)
 
+        self.robot = URDF.from_parameter_server()
+        tree = kdl_tree_from_urdf_model(self.robot)
+
         rospy.Subscriber('flow', OpticalFlow, self.flow_cb)
         rospy.Subscriber('/imu/data', Imu, self.imu_cb)
 
@@ -56,8 +63,37 @@ class FlowTransformer(object):
         self.odom_tf.header.stamp = rospy.Time.now()
 
         rot = quaternion_from_euler(0, 0, self.theta_t)
+        # self.broadcaster.sendTransform(
+        #     self.x_t, rot, rospy.Time.now(), 'flodom', 'odom_temp'
+        # )
+        f = PyKDL.Frame(
+            PyKDL.Rotation.Quaternion(*rot),
+            PyKDL.Vector(*self.x_t)
+        )
+        f_inv = f.Inverse()
+        trans = TransformStamped()
+        trans.header.frame_id = self.flow_frame
+        trans.child_frame_id = 'flow_vector'
+        trans.header.stamp = self.odom_tf.header.stamp
+
+        trans.transform.translation.x = f_inv.p.x()
+        trans.transform.translation.y = f_inv.p.y()
+        trans.transform.translation.z = f_inv.p.z()
+
+        q = f_inv.M.GetQuaternion()
+        trans.transform.rotation.x = q[0]
+        trans.transform.rotation.y = q[1]
+        trans.transform.rotation.z = q[2]
+        trans.transform.rotation.w = q[3]
+
+
+        self.listener.setTransform(trans)
+        new_trans = self.listener.lookupTransform(self.base_frame, 'flow_vector', rospy.Time(0))
+
+        # self.listener.lookupTransform(self.base_frame, self.)
+        print new_trans
         self.broadcaster.sendTransform(
-            self.x_t, rot, rospy.Time.now(), 'flodom', 'odom_temp'
+            new_trans[0], new_trans[1], rospy.Time.now(), 'flooooow', self.base_frame
         )
 
     def imu_cb(self, msg):
@@ -110,7 +146,7 @@ class FlowTransformer(object):
 
 if __name__ == '__main__':
     rospy.init_node('flow_odom')
-    base_frame = rospy.get_param('~base_frame', 'base_link')
+    base_frame = rospy.get_param('~base_frame', 'base_footprint')
     flow_frame = rospy.get_param('~flow_frame', 'flow_cam_link')
     fixed_frame = rospy.get_param('~fixed_frame', 'flodom')
 
