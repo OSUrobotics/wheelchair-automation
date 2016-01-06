3ec0c75dc5dfd438819841a2653dac609db07504
curranw <curranw@onid.orst.edu>
2016-01-06 11:46:48 -0800

    initial checkin for dubai changes

--- wheelchair_description/gazebo_spawn.bash
+++ wheelchair_description/gazebo_spawn.bash
@@ -4,9 +4,9 @@ rosrun xacro xacro.py urdf/wheelchair_gazebo.xacro > urdf/model.urdf
 
 trap 'kill $fooPid $barPid' EXIT
 
-roslaunch gazebo_ros willowgarage_world.launch & fooPid=$!
+roslaunch wheelchair_description wheelchair_world.launch & fooPid=$!
 
-rosrun gazebo_ros spawn_model -file urdf/model.urdf -urdf -model wheelchair & barPid=$!
+rosrun gazebo_ros spawn_model -file urdf/model.urdf -urdf -model wheelchair -x -2 -y 1 & barPid=$!
 
 wait
 
