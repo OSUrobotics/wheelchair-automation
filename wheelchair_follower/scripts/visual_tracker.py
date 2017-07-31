cf8a8e1a90d42d430274b04daba8879e6f4c7319
Benjamin Narin <benjamin.narin@gmail.com>
2017-07-31 13:40:54 -0700

    Inital grabcut working

--- wheelchair_follower/scripts/visual_tracker.py
+++ wheelchair_follower/scripts/visual_tracker.py
@@ -19,8 +19,29 @@ class vfolllower:
             cv_image = self.bridge.imgmsg_to_cv2(stream, "bgr8")
             height = np.size(cv_image, 0)
             width = np.size(cv_image, 1)
-            # print height
-            # print width
+
+            bgdModel = np.zeros((1,65),np.float64)
+            fgdModel = np.zeros((1,65),np.float64)
+
+            #Mask Creation
+            # a = np.ones((480,70))
+            # a = a*cv2.GC_PR_BGD
+            # b = np.ones((480,500))
+            # b = b*cv2.GC_PR_FGD
+            # c = np.ones((480,70))
+            # c = c*cv2.GC_PR_BGD
+
+            # output = np.concatenate((a,b),axis=1)
+            # mask = np.concatenate((output,c),axis=1)
+
+            # print mask.shape
+            mask = np.ones(cv_image.shape[:2],np.uint8)
+            rect = (120,1,450,479)
+            # rect = (161,79,150,150)
+            # cv2.grabCut(cv_image,mask,rect,bgdModel,fgdModel,2,cv2.GC_INIT_WITH_RECT)
+            # mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+            # img_cut = cv_image*mask2[:,:,np.newaxis]
+
 
         except CvBridgeError as e:
             print(e)
@@ -30,6 +51,8 @@ class vfolllower:
         mid_bottom_y = 0
         mid_top_x = np.size(cv_image, 1)/2
         mid_top_y = np.size(cv_image, 0)
+        # cv2.rectangle(cv_image, (120,480),(450,0),(255,0,0), 2)
+        cv2.rectangle(cv_image, (120,1),(450,479),(255,0,0), 2)
         cv2.line(cv_image, (mid_bottom_x,mid_bottom_y), (mid_top_x,mid_top_y), (0,0,255), 2);
         cv2.imshow("Image window", cv_image)
         cv2.waitKey(3)
