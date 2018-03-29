4a2795eeabd757a3f64bd730cadbce1b8b247d2e
Benjamin Narin <benjamin.narin@gmail.com>
2018-03-29 14:02:12 -0700

    Hall World Added

---
+++ wheelchair_description/gazebo_spawn_hall.bash
@@ -0,0 +1,15 @@
+
+
+rosrun xacro xacro.py urdf/wheelchair_gazebo.xacro > urdf/model.urdf
+
+trap 'kill $fooPid $barPid' EXIT
+
+roslaunch wheelchair_description wheelchair_world_hall.launch & fooPid=$!
+
+
+#rosrun gazebo_ros spawn_model -file urdf/model.urdf -urdf -model wheelchair -x -3 -y 0 & barPid=$!
+rosrun gazebo_ros spawn_model -file urdf/model.urdf -urdf -model wheelchair -x -5 -y 0 & barPid=$!
+
+wait
+
+
