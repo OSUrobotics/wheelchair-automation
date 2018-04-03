cc0c9f39cf05d08e07cfa50cc09f3bad4b084ca9
Zachary Lee <zacharyianlee@gmail.com>
2018-04-03 14:28:15 -0700

    Fixed gazebo_full_launch.bash file.

--- wheelchair_description/gazebo_full_launch.bash
+++ wheelchair_description/gazebo_full_launch.bash
@@ -3,13 +3,25 @@
 
 rosrun xacro xacro.py urdf/wheelchair_gazebo.xacro > urdf/model.urdf
 
-trap 'kill $fooPid $barPid $extraPid $rvizPid' EXIT
+trap 'kill $fooPid $barPid $extraPid $rvizPid $spawnPid $mergerPid $movePid' EXIT
 
 roslaunch wheelchair_description wheelchair_world.launch & fooPid=$!
 
 rosrun gazebo_ros spawn_model -file urdf/model.urdf -urdf -model wheelchair -x -2 -y 1 & barPid=$!
 
-roslaunch wheelchair_bringup wheelchair_full_simulation.launch & extraPid=$!
+sleep 10
+
+roslaunch wheelchair_description wheelchair_bringup.launch & spawnPid=$!
+
+sleep 10
+
+roslaunch wheelchair_description wheelchair_merger.launch & mergerPid=$!
+
+sleep 10
+
+roslaunch wheelchair_navigation move_base_local.launch & movePid=$!
+
+sleep 5
 
 rosrun rviz rviz & rvizPid=$!
 wait
