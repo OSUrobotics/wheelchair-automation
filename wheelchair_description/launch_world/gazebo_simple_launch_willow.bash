dbce20426e3c52a985de61158aa3ab7f74a6a758
Benjamin <narinb@oregonstate.edu>
2018-04-30 13:23:54 -0700

    Launch Files and wheelchair_description Cleaned Up

---
+++ wheelchair_description/launch_world/gazebo_simple_launch_willow.bash
@@ -0,0 +1,18 @@
+
+
+rosrun xacro xacro.py urdf/wheelchair_gazebo.xacro > ../urdf/model.urdf
+
+trap 'kill $fooPid $barPid $extraPid $rvizPid $spawnPid $mergerPid $movePid' EXIT
+
+roslaunch wheelchair_description wheelchair_world_willow.launch & fooPid=$!
+
+rosrun gazebo_ros spawn_model -file ../urdf/model.urdf -urdf -model wheelchair -x -2 -y 1 & barPid=$!
+
+sleep 8
+
+roslaunch wheelchair_description wheelchair_bringup.launch & spawnPid=$!
+
+sleep 8
+
+rosrun rviz rviz -d ./WheelchairSetup.rviz & rvizPid=$!
+wait
